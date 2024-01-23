d5f768208c9dbc97837dbe881f41c8660bd35da9
programin.saifulislam@gmail.com <programin.saifulislam@gmail.com>
2024-01-23 22:28:47 +0600

    implemet responsive desing in banner and navbar section

--- src/Components/NavBar/Banner/Banner.jsx
+++ src/Components/NavBar/Banner/Banner.jsx
@@ -2,18 +2,23 @@
 import { MdOutlineFileDownload } from "react-icons/md";
 const Banner = () => {
     return (
-        <div className=" p-5">
+        <div className=" ">
             <div className=" w-full h-auto rounded-2xl p-20 " style={{ backgroundImage: "url(https://i.ibb.co/bb5rKh5/316546300-547692113846445-7299710494491288098-n.png)", backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat" }}>
                 <div className=" flex justify-center items-center gap-10 ">
                     <div className=" flex-1 px-5  space-y-7 text-blue-50">
-                        <h3 className=" text-7xl font-bold  ">Message <br /> privately</h3>
-                        <p className=" text-xl font-semibold">Simple, reliable, private messaging and calling for free*, available all over the world.</p>
+                        <div className=" block lg:hidden">
+                            <img src="https://i.ibb.co/zN1tdxh/329792464-534173148815054-3736500652022655929-n.png" alt="" />
+                            <img className=" relative -top-3 md:-top-4" src="https://i.ibb.co/RzJfVrc/326429760-565901131866976-8960223839941330956-n.png" alt="" />
+                            <img src="https://i.ibb.co/rZBdLHM/318567080-617695970110803-5923371384204745052-n.png" alt="" />
+                        </div>
+                        <h3 className=" text-6xl font-semibold  ">Message <br /> privately</h3>
+                        <p className=" text-xl ">Simple, reliable, private messaging and calling for free*, available all over the world.</p>
                         <button className=" py-3 px-6 bg-blue-900 text-blue-50 border-0 rounded-full flex items-center gap-2 ">Download <span className=" text-2xl"><MdOutlineFileDownload /></span> </button>
                     </div>
-                    <div className=" flex-1 space-y-5">
+                    <div className=" hidden lg:block flex-1 space-y-5">
                         <div className=" space-y-0">
                             <img src="https://i.ibb.co/zN1tdxh/329792464-534173148815054-3736500652022655929-n.png" alt="" />
-                            <img className=" relative -top-5" src="https://i.ibb.co/RzJfVrc/326429760-565901131866976-8960223839941330956-n.png" alt="" />
+                            <img className=" relative -top-4" src="https://i.ibb.co/RzJfVrc/326429760-565901131866976-8960223839941330956-n.png" alt="" />
                             <img src="https://i.ibb.co/rZBdLHM/318567080-617695970110803-5923371384204745052-n.png" alt="" />
 
                         </div>
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 
 import { BiMessageRoundedDots } from "react-icons/bi";
+import { MdOutlineFileDownload } from "react-icons/md";
 const NavBar = () => {
     const manu = <>
     <li>Features</li>
@@ -17,22 +18,22 @@ const NavBar = () => {
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content text-black mt-3 z-[1] p-5 space-y-4 shadow bg-base-100 rounded-box w-52 cursor-pointer">
                         {manu}
                     </ul>
                 </div>
                 <div className=" flex items-centers gap-1">
                     <span className=" text-5xl"><BiMessageRoundedDots /></span>
-                    <p className=" text-3xl font-semibold">MyChat</p>
+                    <p className=" text-3xl font-semibold cursor-pointer">MyChat</p>
                 </div>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1 space-x-8 text-lg">
+                <ul className="menu menu-horizontal px-1 space-x-8 text-lg cursor-pointer">
                     {manu}
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Download</a>
+            <button className=" py-3 px-6 bg-blue-50 text-black border-0 rounded-full  items-center gap-2 hidden md:flex ">Download <span className=" text-2xl"><MdOutlineFileDownload /></span> </button>
             </div>
         </div>
     );
